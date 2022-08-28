65c4385b47516c51da9ebbdb3b5968ccb622a4db
LouisRadek <radek.louis@gmail.com>
2022-08-28 21:36:14 +0200

    feat: Initial commit for the cloud project

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="root">
+    <div class="container">
+      <h1>Login</h1>
+      <form action="">
+        <input class='field1' type="text" placeholder="Username">
+        <input class="field2" type="password" placeholder="password">
+        <button class="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout: "defaultStyles"
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  background: var(--bg-primary);
+  height: 100%;
+}
+
+::selection{
+  background: #6649b8;
+} 
+
+.field1, .field2 {
+  position: absolute;
+}
+
+.submit {
+  background: -webkit-linear-gradient(right, #7101d1, #800fdc, #8e1ae8, #9d23f3, #ac2cff);
+  border-radius: 5px;
+  border: none;
+}
+
+</style>
