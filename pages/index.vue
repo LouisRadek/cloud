d87b4f498e968a7d64c4a42d04996732bc2c5114
LouisRadek <radek.louis@gmail.com>
2022-08-30 22:19:11 +0200

    feat: add styles for login

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <div class="root">
     <div class="container">
       <h1>Login</h1>
-      <form action="">
-        <input class='field1' type="text" placeholder="Username">
-        <input class="field2" type="password" placeholder="password">
-        <button class="submit">Submit</button>
+      <form action="" class="form-inner">
+        <input class='field1' type="text" placeholder=" Username">
+        <input class="field2" type="password" placeholder=" Password">
+        <button class="submit">Login</button>
       </form>
     </div>
   </div>
@@ -27,20 +27,39 @@ export default {
   justify-content: center;
   background: var(--bg-primary);
   height: 100%;
+  max-width: 450px;
+  padding: 40px 50px 65px 50px;
+  border-radius: 40px;
+  margin-top: 200px;
+  box-shadow: 0px 15px 20px rgba(0,0,0,0.1);
 }
 
-::selection{
-  background: #6649b8;
-} 
-
 .field1, .field2 {
-  position: absolute;
+  border: 1px solid lightgray; 
+  outline: none;
+  background: #34343d;
+  border-radius: 5px;
+  margin: 25px;
+  margin-bottom: 1px;
+  width: 65%;
+  transition: all 0.3s ease;
+}
+
+.field1:focus, .field2:focus {
+  border-color: #6649b8;
+}
+
+.field1, .field2 ::placeholder {
+  color: #999;
 }
 
 .submit {
   background: -webkit-linear-gradient(right, #7101d1, #800fdc, #8e1ae8, #9d23f3, #ac2cff);
   border-radius: 5px;
   border: none;
+  cursor: pointer;
+  width: 65%;
+  margin: 25px;
 }
 
 </style>
